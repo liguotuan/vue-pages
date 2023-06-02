491c481d437fa717dec11c57f037f0d3cc1ba99f
liguotuan <1374725370@qq.com>
2023-06-02 20:54:26 +0800

    null

--- src/modules/client/main.js
+++ src/modules/client/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router/index';
+import {
+    createPinia
+} from 'pinia'
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
